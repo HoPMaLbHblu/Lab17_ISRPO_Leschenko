a708ab388c8751dc1b60593206160993796c3d8e
Dan <vfvf5389127@gmail.com>
2026-03-29 13:31:40 +0300

    Step 7 arrays

---
+++ code/Step7_arrayElement/Program.cs
@@ -0,0 +1,6 @@
+﻿int[] arr = { 10, 20, 30, 40, 50 };
+
+for (int i = 0; i < arr.Length; i++)
+{
+    Console.WriteLine(arr[i]);
+}
---
+++ code/Step7_arrayMax/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] arr = { 10, 45, 23, 78, 34, 89, 12 };
+int max = arr[0];
+
+for (int i = 1; i < arr.Length; i++)
+{
+    if (arr[i] > max)
+    {
+        max = arr[i];
+    }
+}
+
+Console.WriteLine($"Максимальный элемент: {max}");
